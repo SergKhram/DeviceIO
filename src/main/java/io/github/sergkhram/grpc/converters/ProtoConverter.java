e5879744af92eba740ec4f088c676a3167083096
SKhramkov <hramkov@dev.vtb>
2022-07-12 12:41:26 +0200

    gRPC implementation continue p2

---
+++ src/main/java/io/github/sergkhram/grpc/converters/ProtoConverter.java
@@ -0,0 +1,83 @@
+package io.github.sergkhram.grpc.converters;
+
+import io.github.sergkhram.data.entity.Host;
+import io.github.sergkhram.data.enums.DeviceType;
+import io.github.sergkhram.data.enums.OsType;
+import io.github.sergkhram.proto.*;
+import org.hibernate.LazyInitializationException;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+public class ProtoConverter {
+    public static List<HostProto> convertHosts(List<Host> hosts) {
+        return hosts.parallelStream()
+            .map(
+                ProtoConverter::convertHostToHostProto
+            )
+            .collect(Collectors.toList());
+    }
+
+    public static HostProto convertHostToHostProto(Host host) {
+        HostProto hostObject = HostProto.newBuilder()
+            .setId(String.valueOf(host.getId()))
+            .setName(host.getName())
+            .setAddress(host.getAddress())
+            .setIsActive(host.getIsActive())
+            .addAllDevices(
+                convertDevices(host)
+            )
+            .build();
+        if(host.getPort()!=null) {
+            hostObject = hostObject.toBuilder().setPort(
+                host.getPort()
+            ).build();
+        }
+        return hostObject;
+    }
+
+    public static Host convertHostProtoRequestToHost(PostHostRequest hostRequest) {
+        Host host = new Host();
+        host.setName(hostRequest.getName());
+        host.setAddress(hostRequest.getAddress());
+        if(hostRequest.getPort() != 0) host.setPort(hostRequest.getPort());
+        return host;
+    }
+
+    public static Host convertUpdateOrDeleteHostProtoRequestToHost(UpdateOrDeleteHostRequest hostRequest) {
+        Host host = new Host();
+        host.setName(hostRequest.getName());
+        host.setAddress(hostRequest.getAddress());
+        if(hostRequest.getPort() != 0) host.setPort(hostRequest.getPort());
+        return host;
+    }
+
+    public static List<DeviceProto> convertDevices(Host host) {
+        try {
+            return Objects.requireNonNull(host.getDevices()).parallelStream().map(
+                it -> DeviceProto.newBuilder()
+                    .setDeviceType(
+                        it.getDeviceType().equals(DeviceType.DEVICE)
+                            ? DeviceTypeProto.DEVICE
+                            : DeviceTypeProto.SIMULATOR
+                    )
+                    .setId(String.valueOf(it.getId()))
+                    .setName(it.getName())
+                    .setIsActive(it.getIsActive())
+                    .setOsType(
+                        it.getOsType().equals(OsType.ANDROID)
+                            ? OsTypeProto.ANDROID
+                            : OsTypeProto.IOS
+                    )
+                    .setSerial(it.getSerial())
+                    .setState(it.getState())
+                    .setOsVersion(it.getOsVersion())
+                    .build()
+            ).collect(Collectors.toList());
+        } catch (LazyInitializationException e) {
+            return Collections.emptyList();
+        }
+    }
+}
--- src/main/java/io/github/sergkhram/grpc/services/HostsGrpcService.java
+++ src/main/java/io/github/sergkhram/grpc/services/HostsGrpcService.java
@@ -1,19 +1,17 @@
 package io.github.sergkhram.grpc.services;
 
 import com.google.protobuf.Empty;
-import io.github.sergkhram.data.enums.DeviceType;
-import io.github.sergkhram.data.enums.OsType;
 import io.github.sergkhram.proto.*;
 import io.github.sergkhram.logic.HostRequestsService;
 import io.github.sergkhram.data.entity.Host;
 import io.grpc.stub.StreamObserver;
 import lombok.extern.slf4j.Slf4j;
 import net.devh.boot.grpc.server.service.GrpcService;
-import org.hibernate.LazyInitializationException;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.*;
-import java.util.stream.Collectors;
+
+import static io.github.sergkhram.grpc.converters.ProtoConverter.*;
 
 @GrpcService
 @Slf4j
@@ -90,81 +88,12 @@ public class HostsGrpcService extends  HostsServiceGrpc.HostsServiceImplBase {
             Host host = hostRequestsService.getHostInfo(request.getId());
             hostRequestsService.deleteHost(host);
 
-            Empty response = Empty.newBuilder().build();
+            Empty response = Empty.getDefaultInstance();
 
             responseObserver.onNext(response);
             responseObserver.onCompleted();
         } catch (Exception e) {
             responseObserver.onError(e);
-        }
-    }
-
-    private List<HostProto> convertHosts(List<Host> hosts) {
-        return hosts.parallelStream()
-            .map(
-                this::convertHostToHostProto
-            )
-            .collect(Collectors.toList());
-    }
-
-    private HostProto convertHostToHostProto(Host host) {
-        HostProto hostObject = HostProto.newBuilder()
-            .setId(String.valueOf(host.getId()))
-            .setName(host.getName())
-            .setAddress(host.getAddress())
-            .setIsActive(host.getIsActive())
-            .addAllDevices(
-                convertDevices(host)
-            )
-            .build();
-        if(host.getPort()!=null) {
-            hostObject = hostObject.toBuilder().setPort(
-                host.getPort()
-            ).build();
-        }
-        return hostObject;
-    }
-
-    private Host convertHostProtoRequestToHost(PostHostRequest hostRequest) {
-        Host host = new Host();
-        host.setName(hostRequest.getName());
-        host.setAddress(hostRequest.getAddress());
-        if(hostRequest.getPort() != 0) host.setPort(hostRequest.getPort());
-        return host;
-    }
-
-    private Host convertUpdateOrDeleteHostProtoRequestToHost(UpdateOrDeleteHostRequest hostRequest) {
-        Host host = new Host();
-        host.setName(hostRequest.getName());
-        host.setAddress(hostRequest.getAddress());
-        if(hostRequest.getPort() != 0) host.setPort(hostRequest.getPort());
-        return host;
-    }
-
-    private List<DeviceProto> convertDevices(Host host) {
-        try {
-            return Objects.requireNonNull(host.getDevices()).parallelStream().map(
-                it -> DeviceProto.newBuilder()
-                    .setDeviceType(
-                        it.getDeviceType().equals(DeviceType.DEVICE)
-                            ? DeviceTypeProto.DEVICE
-                            : DeviceTypeProto.SIMULATOR
-                    )
-                    .setId(String.valueOf(it.getId()))
-                    .setName(it.getName())
-                    .setIsActive(it.getIsActive())
-                    .setOsType(
-                        it.getOsType().equals(OsType.ANDROID)
-                            ? OsTypeProto.ANDROID
-                            : OsTypeProto.IOS
-                    )
-                    .setSerial(it.getSerial())
-                    .setState(it.getState())
-                    .setOsVersion(it.getOsVersion())
-                    .build()
-            ).collect(Collectors.toList());
-        } catch (LazyInitializationException e) {
-            return Collections.emptyList();
         }
     }
 }
