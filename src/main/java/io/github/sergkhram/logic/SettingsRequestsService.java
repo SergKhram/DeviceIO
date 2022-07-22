5c171158434fec52b245a9f17dcd3dcc0406036e
SKhramkov <hramkov@dev.vtb>
2022-07-22 13:51:02 +0200

    Fixed the settings view

--- src/main/java/io/github/sergkhram/logic/SettingsRequestsService.java
+++ src/main/java/io/github/sergkhram/logic/SettingsRequestsService.java
@@ -2,8 +2,12 @@ package io.github.sergkhram.logic;
 
 import io.github.sergkhram.data.entity.Settings;
 import io.github.sergkhram.data.service.CrmService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
+@Slf4j
 public class SettingsRequestsService {
     @Autowired
     CrmService crmService;
